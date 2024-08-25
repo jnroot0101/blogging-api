1616939c9d2917e46096fb9d2501faaf08371059
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2024-08-25 20:00:21 +0100

    fix bug

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getPosts, getPost, createPost, updatePost, deletPost, deletePost} = require('../controllers/posts');
+const {getPosts, getPost, createPost, updatePost, deletePost} = require('../controllers/posts');
 
 router.get('/', getPosts);
 router.get('/:id', getPost);
